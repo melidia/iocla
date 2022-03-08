ee00f54394402ee2ef7923242ce739c1c8fc17a2
Lidia Manea <manealidia23@yahoo.com>
2022-03-08 21:58:20 +0200

    lab1: ex5: Added solution for exercise 5
    Signed-off-by: Lidia Manea <manealidia23@yahoo.com>

--- laborator/content/reprezentare-numere/5-odd_even/odd_even.c
+++ laborator/content/reprezentare-numere/5-odd_even/odd_even.c
@@ -3,22 +3,42 @@
 
 void print_binary(int number, int nr_bits)
 {
-	/* TODO */
-	(void) number;
-	(void) nr_bits;
+	printf("0b");
+	for(int i = nr_bits / 4 - 1; i >= 0; --i) {
+		int k = number >> i;
+        if (k & 1)
+            printf("1");
+        else
+            printf("0");
+    }
 }
 
 void check_parity(int *numbers, int n)
 {
-	/* TODO */
-	(void) numbers;
-	(void) n;
+	for(int i = 0; i < n; ++i) {
+		if(!(*(numbers + i) & 1)) {
+			print_binary(*(numbers + i), 32);
+			printf("\n");
+		}
+		else {
+			printf("0x");
+			printf("%x", *(numbers + i));
+			printf("\n");
+		}
+	}
 }
 
-int main()
+int main(void)
 {
-	/* TODO: Test functions */
-
+	int n;
+	scanf("%d", &n);
+	int *v = malloc(n * sizeof(int));
+	int i = 0;
+	while(i < n) {
+		scanf("%d", v + i);
+		++i;
+	}
+	check_parity(v, n);
 	return 0;
 }
 
