56bc5b2a6121d829bf5da60a16885fc57581a1d8
Lidia Manea <manealidia23@yahoo.com>
2022-03-08 21:55:16 +0200

    lab1: ex2: Added solution for exercise 2
    Signed-off-by: Lidia Manea <manealidia23@yahoo.com>

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -4,21 +4,32 @@
 
 int my_strlen(const char *str)
 {
-	/* TODO */
 	(void) str;
-
+	int c = 0;
+	if(str != NULL) {
+		while(*str != '\0') {
+			c++;
+			str++;
+		}
+	return c;
+	}
 	return -1;
 }
 
 void equality_check(const char *str)
 {
-	/* TODO */
 	(void) str;
+	int len = my_strlen(str);
+	for(int i = 0; i < len; ++i) {
+		if(!(*(str + i) ^ *(str + (i + (1<<i))%len))) {
+			printf("%p\n", str + i);
+		}
+	}
 }
 
 int main(void)
 {
-	/* TODO: Test functions */
+	equality_check("mamaaremere");
 
 	return 0;
 }
