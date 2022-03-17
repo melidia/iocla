2c8014b8bc1ef120922149c767f7e01c3a7f178a
Lidia Manea <manealidia23@yahoo.com>
2022-03-17 11:03:57 +0200

    Lab2: ex2: Added solution for exercise 2
    Signed-off-by: Lidia Manea <manealidia23@yahoo.com>

--- laborator/content/operatii-memorie-gdb/ex2/ex2.c
+++ laborator/content/operatii-memorie-gdb/ex2/ex2.c
@@ -2,14 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern) {
+	int len1 = strlen(s);
+	int len2 = strlen(pattern);
+	int poz;
+	int found = 0;
+	for(int i = 0; i <= len1 - len2; ++i){
+		found = 1;
+		poz = i;
+		for(int j = i; j < i + len2; ++j){
+			if(s[j] != pattern[j - i]){
+				found = 0;
+				poz = -1;
+				break;
+			}
+		}
+	if(found == 1)
+		break;
+	}
+	if(found == 0)
+		return s;
+	else {
+	char *t = malloc(len1 - len2);
+	for(int i = 0; i < poz; ++i)
+		t[i] = s[i];
+	for(int i = poz + len2; i < len1; ++i)
+		t[i - len2] = s[i];
+	return t;
+}
+}
 
 int main(){
 	char *s = "Ana are mere";
 	char *pattern = "re";
-
-	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
