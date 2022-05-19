a768a9793119f49ebafd854321dfc31402c81f5b
Lidia Manea <manealidia23@yahoo.com>
2022-05-19 22:09:31 +0300

    Ex45 solved
    Signed-off-by: Lidia Manea <manealidia23@yahoo.com>

--- laborator/content/interactiune-c-assembly/4-5-max-assembly-calls/main.asm
+++ laborator/content/interactiune-c-assembly/4-5-max-assembly-calls/main.asm
@@ -4,6 +4,7 @@ extern get_max
 section .data
     arr: dd 19, 7, 129, 87, 54, 218, 67, 12, 19, 99
     len: equ $-arr
+    pos: dd 0
 
     print_format: db "max: %u", 13, 10, 0
 
@@ -21,16 +22,18 @@ main:
     mov eax, len
     shr eax, 2
 
+    push pos
     push eax
     push arr
     call get_max
-    add esp, 8
+    add esp, 12
 
     ; Print max.
+    push dword [pos]
     push eax
     push print_format
     call printf
-    add esp, 8
+    add esp, 12
 
     leave
     ret
