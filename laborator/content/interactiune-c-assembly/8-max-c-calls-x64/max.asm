bcb636a69e8261f5abcb9e568b39073bb357434a
Lidia Manea <manealidia23@yahoo.com>
2022-05-19 22:10:35 +0300

    Ex8 solved
    Signed-off-by: Lidia Manea <manealidia23@yahoo.com>

--- laborator/content/interactiune-c-assembly/8-max-c-calls-x64/max.asm
+++ laborator/content/interactiune-c-assembly/8-max-c-calls-x64/max.asm
@@ -3,22 +3,24 @@ section .text
 global get_max
 
 get_max:
-	push ebp
-	mov ebp, esp
+	push rbp
+	mov rbp, rsp
 
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
 
-	mov ebx, [ebp+8]
-	mov ecx, [ebp+12]
+	mov ebx, [ebp+8] RDI
+	mov ecx, [ebp+12] RSI
 	xor eax, eax
+	mov rcx, rsi
 
 compare:
-	cmp eax, [ebx+ecx*4-4]
+	cmp eax, [rdi+rcx*4-4]
 	jge check_end
-	mov eax, [ebx+ecx*4-4]
+	mov eax, [rdi+rcx*4-4]
 check_end:
 	loopnz compare
+	pop rbx
 
 	leave
 	ret
